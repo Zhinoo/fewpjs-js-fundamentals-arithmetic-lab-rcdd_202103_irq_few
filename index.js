70450243ba89c5f19ec9f8598db62f052dc27c42
Zhino Salam <zhinosalam188@gmail.com>
2021-06-09 08:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your code here
-let var1 = 31, var2 =2;
-const multiply = var1 * var2;
-const random = Math.floor(Math.random() * 10) + 1;
-const mod = 32%7;
-const max = Math.max( 5, 10, 15, 20 )
+let var1 = 31
+let var2 = 2
+const multiply = var1 * var2
+
+// Write your code here 
+const random = Math.random()
+
+const mod = 9 % 5
+
+const max = Math.max(1, 2, 20)
